afce541ab43b9cb1585b492c3303b75e61881a29
AsadNawazgsoftconsulting <asadnawaz@gsoftconsulting.com>
2025-02-19 11:08:13 +0500

    remove debug

--- voice.py
+++ voice.py
@@ -8,8 +8,6 @@ from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 from dotenv import load_dotenv
 import os
 
-print("Loading...")
-
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
